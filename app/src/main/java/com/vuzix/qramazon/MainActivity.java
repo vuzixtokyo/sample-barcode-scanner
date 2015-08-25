5e074e0cef623d2eba85ecdeb4d2c2e73d5f49d5
Keiji Ariyama <keiji_ariyama@c-lis.co.jp>
2015-08-25 18:46:20 +0900

    BarCodeの読み込みに対応した。

--- app/src/main/java/com/vuzix/qramazon/MainActivity.java
+++ app/src/main/java/com/vuzix/qramazon/MainActivity.java
@@ -1,37 +1,129 @@
 package com.vuzix.qramazon;
 
+import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.PlanarYUVLuminanceSource;
+import com.google.zxing.ReaderException;
+import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
+
+import java.io.IOException;
+
+public class MainActivity extends Activity implements SurfaceHolder.Callback {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Camera mCamera;
+
+    private final Camera.AutoFocusCallback mAutoFocusCallback = new Camera.AutoFocusCallback() {
+
+        @Override
+        public void onAutoFocus(boolean success, Camera camera) {
+            if (success) {
+                mCamera.setOneShotPreviewCallback(mPreviewCallback);
+            } else {
+                Toast.makeText(getApplicationContext(), "focus failed.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    private final Camera.PreviewCallback mPreviewCallback = new Camera.PreviewCallback() {
+        @Override
+        public void onPreviewFrame(byte[] data, Camera camera) {
+            Camera.Size size = camera.getParameters().getPreviewSize();
+            PlanarYUVLuminanceSource source = new PlanarYUVLuminanceSource(data, size.width,
+                    size.height, 0, 0, size.width, size.height, false);
+
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+            MultiFormatReader multiFormatReader = new MultiFormatReader();
+            try {
+                Result rawResult = multiFormatReader.decode(bitmap);
+
+                Toast.makeText(getApplicationContext(), rawResult.getText(), Toast.LENGTH_LONG)
+                        .show();
+            } catch (ReaderException e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        Log.d(TAG, "onCreate");
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+
+        SurfaceView surfaceView = new SurfaceView(this);
+        surfaceView.getHolder().addCallback(this);
+
+        setContentView(surfaceView);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        Log.d(TAG, "surfaceCreated");
+
+        mCamera = Camera.open();
+
+        try {
+            mCamera.setPreviewDisplay(surfaceHolder);
+            mCamera.setDisplayOrientation(180); // inverse
+
+        } catch (IOException e) {
+            Log.e(TAG, "IOException", e);
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
+        Log.d(TAG, "surfaceChanged");
+        mCamera.startPreview();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        Log.d(TAG, "surfaceDestroyed");
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_ENTER:
+                mCamera.autoFocus(mAutoFocusCallback);
+                break;
         }
 
-        return super.onOptionsItemSelected(item);
+        return super.onKeyDown(keyCode, event);
     }
 }
